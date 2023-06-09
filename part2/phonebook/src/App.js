3c65d2a932f6ae066776e3c39215605cc1890ef1
Tturna <42873350+Tturna@users.noreply.github.com>
2023-06-09 18:05:33 +0300

    exercises 2.16-2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import DBAccess from './services/DBAccess'
+import './index.css'
 
 const Filter = ({filterString, filterChangeCallback}) => {
   return(
@@ -28,39 +29,38 @@ const AddForm = (props) => {
   )
 }
 
-const DeleteButton = (props) => {
-  const {name, _, id} = props.person
-  const [persons, setPersons] = props.hook
-
-  const deleteEntry = () => {
-    if (!window.confirm(`Delete ${name}?`)) return;
-
-    DBAccess.deleteEntry(id)
-      .then(() => {
-        // console.log(data)
-        setPersons(persons.filter(p => p.id !== id))
-      })
-  }
+const Notification = ({message}) => {
+  if (message[0] === null) return null;
+  
+  const style = message[1] ? 'error' : 'notification'
   return(
-    <button onClick={() => deleteEntry()}>Delete</button>
+    <p className={style}>{message}</p>
   )
 }
 
-const PersonEntry = (props) => {
-  const {name, number, _} = props.person
-
+const DeleteButton = ({deleteFunction, person: {id}}) => {
   return(
-    <p>
-      {name} {number} <DeleteButton person={props.person} hook={props.hook} />
-    </p>
+    <button onClick={() => deleteFunction(id)}>Delete</button>
   )
 }
 
-const Content = ({visiblePersons, hook}) => {
+// Nested destructuring
+const PersonEntry = ({person: {name, number}}) => <>{name} {number} </>
+
+const Content = ({visiblePersons, deleteFunction}) => {
   return(
     <div>
       <h2>Numbers</h2>
-      {visiblePersons.map(person => <PersonEntry key={person.name} person={person} hook={hook} />)}
+      {
+        visiblePersons.map(person => {
+          return(
+            <p key={person.name}>
+              <PersonEntry person={person} />
+              <DeleteButton person={person} deleteFunction={deleteFunction} />
+            </p>
+          )
+        })
+      }
     </div>
   )
 }
@@ -70,17 +70,26 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState([null, false])
 
   // An effect hook takes two parameters. The first parameter is a function, which React will call,
-  // when the component is rendered for the first time. The second parameter is an array of dependencies,
-  // which controls when the effect is called. If the second parameter is an empty array,
-  // then the effect is only called along with the first render of the component.
+  // when the component is rendered for the first time. The second parameter is an array of variables,
+  // which controls when the effect is called. Whenever a variable in the array changes value,
+  // the effect is executed again.
   useEffect(() => {
     DBAccess.readAll()
       .then(data => {
         setPersons(data)
       })
   }, [])
+
+  const notify = (notification, time) => {
+    setNotification(notification)
+
+    setTimeout(() => {
+      setNotification([null, false])
+    }, time);
+  }
 
   const addName = (event) => {
     event.preventDefault()
@@ -95,16 +104,33 @@ const App = () => {
 
       DBAccess.update(existing.id, nameEntry)
         .then(data => {
-          console.log(data)
+          // console.log(data)
           setPersons(persons.map(p => p.id === data.id ? data : p))
+          notify([`Updated number for ${existing.name}`, false], 5000)
       })
     }
     else {
       DBAccess.create(nameEntry)
         .then(data => {
           setPersons(persons.concat(data))
+          notify([`Added ${nameEntry.name}`, false], 5000)
       })
     }
+  }
+
+  const deleteName = (id) => {
+    const name = persons.find(p => p.id === id).name
+    if (!window.confirm(`Delete ${name}?`)) return;
+
+    DBAccess.deleteEntry(id)
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== id))
+        notify([`Deleted ${name}`, false], 5000)
+      })
+      .catch(error => {
+        console.log(error)
+        notify([`Data for ${name} doesn't exist. Please refresh the page.`, true], 5000)
+      })
   }
   
   const visiblePersons = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
@@ -113,6 +139,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
+      <Notification message={notification} />
       <Filter filterString={filter} filterChangeCallback={event => setFilter(event.target.value)} />
 
       <AddForm
@@ -123,7 +150,7 @@ const App = () => {
         submitFunction={addName}
       />
 
-      <Content visiblePersons={visiblePersons} hook={[persons, setPersons]} />
+      <Content visiblePersons={visiblePersons} deleteFunction={deleteName} />
 
     </div>
   )
