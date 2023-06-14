23dae33ad19506a818574b19be0975dbcf27ccdf
Tturna <42873350+Tturna@users.noreply.github.com>
2023-06-14 10:22:40 +0300

    exercises 2.19-2.20

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,8 +3,41 @@ import axios from "axios"
 
 const Results = ({results}) => {
     if (results === null) return null;
-    if (results.length === 1) return results;
-    return <p>{results}</p>
+    // if (results.length === 1) return results;
+    // return <p>{results}</p>
+    return results
+}
+
+const WeatherInfo = ({country}) => {
+    const [weatherData, setWeatherData] = useState(null)
+    const [lat, lng] = country.capitalInfo.latlng
+    
+    console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            .then(response => {
+                setWeatherData(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+                alert("Something went wrong fetching the weather")
+            })
+    }, [lat, lng])
+
+    if (weatherData === null) return null;
+
+    const celsius = (weatherData.main.temp - 273.15).toFixed(2)
+
+    return(
+        <div>
+            <h1>Weather in {country.capital[0]}</h1>
+            <p>Temperature: {celsius} Celsius</p>
+            <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="weather icon" />
+            <p>Wind: {weatherData.wind.speed} m/s</p>
+        </div>
+    )
 }
 
 const CountryInfo = ({country}) => {
@@ -16,10 +49,19 @@ const CountryInfo = ({country}) => {
             <br />
             Languages:
             <ul>
-                {Object.values(country.languages).map(l => <li>{l}</li>)}
+                {Object.values(country.languages).map(l => <li key={l}>{l}</li>)}
             </ul>
             <img src={country.flags.png} alt={country.flags.alt} />
+
+            <WeatherInfo country={country}/>
+
         </div>
+    )
+}
+
+const CountryListItem = ({country, showCallback}) => {
+    return(
+        <>{country.name.common} <button onClick={showCallback}>Show</button> <br/></>
     )
 }
 
@@ -55,7 +97,7 @@ function App() {
 
                 if (matches.length > 10)
                 {
-                    setResult('Too many matches, specify another filter.')
+                    setResult(<p>'Too many matches, specify another filter.'</p>)
                     return
                 }
             }
@@ -65,7 +107,11 @@ function App() {
             if (matches.length === 1) {
                 setResult(<CountryInfo country={matches[0]} />)
             } else {
-                setResult(matches.map(country => <>{country.name.common}<br/></>))
+                setResult(matches.map(country => {
+                    return(
+                        <CountryListItem key={country.name.common} country={country} showCallback={() => setResult(<CountryInfo country={country} />)} />
+                    )
+                }))
             }
         }
     }
@@ -73,7 +119,7 @@ function App() {
     return (
         <>
             <form>
-            find countries <input value={query} onChange={e => updateSearch(e.target.value)} />
+            find countries <input value={query} onChange={e => updateSearch(e.target.value)} /> <br />
             <Results results={result} />
             </form>
         </>
