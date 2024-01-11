7db9ce209e04fb961cd4d94e7cc244dbfb70ed83
Tturna <42873350+Tturna@users.noreply.github.com>
2024-01-11 21:37:30 +0200

    exercise 4.16 functionality. tests remain

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        // minlength: [3, 'Username must be at least 3 characters long.']
+    },
+    name: String,
+    passwordHash: String
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set('toJSON', {
+    transform: (doc, obj) => {
+        obj.id = obj._id.toString();
+        delete obj._id;
+        delete obj.__v;
+        delete obj.passwordHash;
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
