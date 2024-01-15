48faf86e04ee29b9265d8d0bbc94bcd31c5bb80c
Tturna <42873350+Tturna@users.noreply.github.com>
2024-01-15 16:39:08 +0200

    exercise 5.5

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newTitle, setNewTitle] = useState('')
-  const [newAuthor, setNewAuthor] = useState('')
-  const [newUrl, setNewUrl] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [userData, setUserData] = useState(null)
+  const blogFromRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -20,23 +20,14 @@ const App = () => {
     )  
   }, [])
 
-  const addBlog = async (event) => {
-    event.preventDefault()
-    const newBlog = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl
-    }
-
+  const createBlog = async (newBlog) => {
     const returnedBlog = await blogService.create(newBlog)
+    blogFromRef.current.toggleVisibility()
     setBlogs(blogs.concat(returnedBlog))
-    setNewTitle('')
-    setNewAuthor('')
-    setNewUrl('')
 
     setNotificationMessage(`Added blog '${returnedBlog.title}' by ${returnedBlog.author}`)
     setTimeout(() => {
-      setNotificationMessage(null)
+        setNotificationMessage(null)
     }, 5000);
   }
 
@@ -48,7 +39,6 @@ const App = () => {
         username, password
       })
 
-      console.log(`response from login: ${responseData}`)
       blogService.setToken(responseData.token)
       window.localStorage.setItem('loggeduser', JSON.stringify(responseData))
       setUserData(responseData)
@@ -112,34 +102,9 @@ const App = () => {
       <p style={{color: 'red'}}>{errorMessage}</p>
       <p style={{color: 'green'}}>{notificationMessage}</p>
 
-      <h3>New Blog</h3>
-      <form onSubmit={addBlog}>
-        <div>
-          Title 
-          <input
-            type='text'
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          Author 
-          <input
-            type='text'
-            value={newAuthor}
-            onChange={(e) => setNewAuthor(e.target.value)}
-          />
-        </div>
-        <div>
-          Url 
-          <input
-            type='text'
-            value={newUrl}
-            onChange={(e) => setNewUrl(e.target.value)}
-          />
-        </div>
-        <button type='submit'>Create</button>
-      </form>
+      <Togglable buttonLabel='New Blog' ref={blogFromRef}>
+        <BlogForm createBlog={createBlog} />
+      </Togglable>
       
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
