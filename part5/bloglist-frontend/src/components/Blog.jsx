8084d9d3380462bb8369aeb1f9f31591993583c6
Tturna <42873350+Tturna@users.noreply.github.com>
2024-01-15 18:15:39 +0200

    exercise 5.7. 5.6 was already done. The problem for 5.8 doesn't exist

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailVisible, setDetailVisible] = useState(false)
+  const showWhenOpen = { display: detailVisible ? '' : 'none' }
+
+  const toggleDetail = () => setDetailVisible(!detailVisible)
+  console.log(blog)
+
+  const blogDiv = {
+    border: 'solid 2px',
+    margin: '10px auto',
+    padding: '5px'
+  }
+
+  const blogP = {
+    display: 'inline-block',
+    margin: '5px 10px 5px 0'
+  }
+
+  const block = { display: 'block' }
+
+  return (
+    <div style={blogDiv}>
+      <p style={blogP}>&apos;{blog.title}&apos; by {blog.author}</p>
+      <div style={showWhenOpen}>
+        <a href={blog.url} style={block}>{blog.url}</a>
+        <p style={blogP}>likes: {blog.likes}</p>
+        <button>Like</button><br/>
+        <p style={blogP}>Added by {blog.creator.username}</p>
+      </div>
+      <button onClick={toggleDetail}>{detailVisible ? 'Hide' : 'View'}</button>
+    </div>  
+  )
+}
 
 export default Blog
