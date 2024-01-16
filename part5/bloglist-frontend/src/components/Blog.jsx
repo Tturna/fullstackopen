d8f30152640df08426b4f5282ccb2b6a11386dc4
Tturna <eemelijuponaho@gmail.com>
2024-01-16 12:06:04 +0200

    exercise 5.12 2/2

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -2,57 +2,57 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Blog = ({ blog, updateBlog, removeBlog, loggedUser }) => {
-  const [detailVisible, setDetailVisible] = useState(false)
-  const showWhenOpen = { display: detailVisible ? '' : 'none' }
-  const loggedIsOwner = blog.creator.username === loggedUser
+    const [detailVisible, setDetailVisible] = useState(false)
+    const showWhenOpen = { display: detailVisible ? '' : 'none' }
+    const loggedIsOwner = blog.creator.username === loggedUser
 
-  const toggleDetail = () => setDetailVisible(!detailVisible)
+    const toggleDetail = () => setDetailVisible(!detailVisible)
 
-  const handleLike = () => {
-    const newBlog = { ...blog }
-    newBlog.likes = blog.likes + 1
-    updateBlog(newBlog)
-  }
-
-  const handleRemove = () => {
-    if (window.confirm(`Are you sure you want to delete '${blog.title}' by ${blog.author}?`)) {
-      removeBlog(blog.id)
+    const handleLike = () => {
+        const newBlog = { ...blog }
+        newBlog.likes = blog.likes + 1
+        updateBlog(newBlog)
     }
-  }
 
-  const blogDiv = {
-    border: 'solid 2px',
-    margin: '10px auto',
-    padding: '5px'
-  }
+    const handleRemove = () => {
+        if (window.confirm(`Are you sure you want to delete '${blog.title}' by ${blog.author}?`)) {
+            removeBlog(blog.id)
+        }
+    }
 
-  const blogP = {
-    display: 'inline-block',
-    margin: '5px 10px 5px 0'
-  }
+    const blogDiv = {
+        border: 'solid 2px',
+        margin: '10px auto',
+        padding: '5px'
+    }
 
-  const block = { display: 'block' }
+    const blogP = {
+        display: 'inline-block',
+        margin: '5px 10px 5px 0'
+    }
 
-  return (
-    <div style={blogDiv}>
-      <p style={blogP}>&apos;{blog.title}&apos; by {blog.author}</p>
-      <div style={showWhenOpen}>
-        <a href={blog.url} style={block}>{blog.url}</a>
-        <p style={blogP}>likes: {blog.likes}</p>
-        <button onClick={handleLike}>Like</button><br/>
-        <p style={blogP}>Added by {blog.creator.username}</p><br/>
-        {loggedIsOwner && <button onClick={handleRemove}>Delete</button>}
-      </div>
-      <button onClick={toggleDetail}>{detailVisible ? 'Hide' : 'View'}</button>
-    </div>
-  )
+    const block = { display: 'block' }
+
+    return (
+        <div style={blogDiv}>
+            <p style={blogP}>&apos;{blog.title}&apos; by {blog.author}</p>
+            <div style={showWhenOpen}>
+                <a href={blog.url} style={block}>{blog.url}</a>
+                <p style={blogP}>likes: {blog.likes}</p>
+                <button onClick={handleLike}>Like</button><br/>
+                <p style={blogP}>Added by {blog.creator.username}</p><br/>
+                {loggedIsOwner && <button onClick={handleRemove}>Delete</button>}
+            </div>
+            <button onClick={toggleDetail}>{detailVisible ? 'Hide' : 'View'}</button>
+        </div>
+    )
 }
 
 Blog.propTypes = {
-  blog: PropTypes.object.isRequired,
-  updateBlog: PropTypes.func.isRequired,
-  removeBlog: PropTypes.func.isRequired,
-  loggedUser: PropTypes.string.isRequired
+    blog: PropTypes.object.isRequired,
+    updateBlog: PropTypes.func.isRequired,
+    removeBlog: PropTypes.func.isRequired,
+    loggedUser: PropTypes.string.isRequired
 }
 
 export default Blog
--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,32 +1,32 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
 
 const Togglable = forwardRef((props, refs) => {
-  const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false)
 
-  const showWhenClosed = { display: visible ? 'none' : '' }
-  const showWhenOpen = { display: visible ? '' : 'none' }
+    const showWhenClosed = { display: visible ? 'none' : '' }
+    const showWhenOpen = { display: visible ? '' : 'none' }
 
-  const toggleVisibility = () => {
-    setVisible(!visible)
-  }
-
-  useImperativeHandle(refs, () => {
-    return {
-      toggleVisibility
+    const toggleVisibility = () => {
+        setVisible(!visible)
     }
-  })
 
-  return (
-    <div>
-      <div style={showWhenClosed}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
-      </div>
-      <div style={showWhenOpen}>
-        {props.children}
-        <button onClick={toggleVisibility}>Cancel</button>
-      </div>
-    </div>
-  )
+    useImperativeHandle(refs, () => {
+        return {
+            toggleVisibility
+        }
+    })
+
+    return (
+        <div>
+            <div style={showWhenClosed}>
+                <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+            </div>
+            <div style={showWhenOpen}>
+                {props.children}
+                <button onClick={toggleVisibility}>Cancel</button>
+            </div>
+        </div>
+    )
 })
 
 Togglable.displayName = 'Togglable'
