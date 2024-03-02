30ad405463d1530eba1c6569fbbe87a10474527b
Tturna <eemelijuponaho@gmail.com>
2024-03-02 12:06:49 +0200

    fixed gitlink, oops. exercise 9.21

---
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import patients from "../../services/patients";
+import { Patient } from "../../types";
+
+const PatientPage = () => {
+    const [patient, setPatient] = useState<Patient>();
+    const id = useParams().id as string;
+    
+    useEffect(() => {
+        patients.getById(id)
+        .then(resultPatient => {
+            setPatient(resultPatient);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, [id]);
+
+    if (!patient) {
+        return <div>Loading...</div>;
+    }
+
+    return(
+        <div>
+            <h2>{patient.name}</h2>
+            <p>Gender: {patient.gender}</p>
+            <p>SSN: {patient.ssn}</p>
+            <p>Occupation: {patient.occupation}</p>
+            <p>Date of birth: {patient.dateOfBirth}</p>
+        </div>
+    );
+};
+
+export default PatientPage;
